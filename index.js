6614af5c3fc25a7fb431dac974e960c4b43297a2
Jan Graf <Jan.Graf@m-tec.com>
2024-04-13 18:29:04 +0200

    created frontend and functions to send data

---
+++ index.js
@@ -0,0 +1,74 @@
+/**
+ * Open a dialog to select an image from the device
+ */
+function selectPhoto() {
+    // trigger the click event of the input to select a photo
+    document.getElementById("image-input").click();
+}
+
+function takePhoto() {
+    var constraints = { video: true };
+
+    navigator.mediaDevices.getUserMedia(constraints)
+        .then(function(mediaStream) {
+            var video = document.createElement('video');
+            video.srcObject = mediaStream;
+            video.onloadedmetadata = function(e) {
+                video.play();
+                var canvas = document.createElement('canvas');
+                canvas.width = video.videoWidth;
+                canvas.height = video.videoHeight;
+                var context = canvas.getContext('2d');
+                context.drawImage(video, 0, 0, canvas.width, canvas.height);
+                var photo = canvas.toDataURL('image/jpeg');
+                showPhotoPreview(photo);
+                mediaStream.getTracks().forEach(track => track.stop());
+            };
+        })
+        .catch(function(err) {
+            console.error('Error accessing camera:', err);
+        });
+}
+
+function showPreview() {
+    // get the input element and select the first image
+    const input = document.getElementById("image-input");
+    const image = input.files[0];
+    // get the preview image and set the src attribute
+    const imgTag = document.getElementById("preview");
+    imgTag.src = URL.createObjectURL(image);
+    // show submit button
+    const submitButton = document.getElementById("submit-btn");
+    submitButton.style.visibility = "visible";
+}
+
+function submit() {
+    // image to submit
+    const input = document.getElementById("image-input");
+    const image = input.files[0];
+
+    // API URL
+    const url = "http://localhost:8000";
+
+    // create FormData and add image
+    const formData = new FormData();
+    formData.append("image", image);
+
+    // send POST-Request
+    fetch(url, {
+        method: "POST",
+        body: formData
+    })
+    .then(response => {
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
+        }
+        return response.json();
+    })
+    .then(data => {
+        console.log('Response:  ', data);
+    })
+    .catch(error => {
+        console.error('Error:', error);
+    });
+}

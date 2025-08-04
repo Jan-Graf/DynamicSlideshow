e8687680959fcf142eb111a71b5247dcf797dbd7
Jan Graf <Jan.Graf@m-tec.com>
2025-08-04 20:49:18 +0200

    Added try except to prevent crashing

--- ImageServer.py
+++ ImageServer.py
@@ -38,39 +38,44 @@ class RequestHandler(BaseHTTPRequestHandler):
             self.send_error(404, 'File Not Found: %s' % self.path)
 
     def do_POST(self):
-        # parse the content type header to get the boundary
-        content_type = self.headers['Content-Type']
-        boundary = content_type.split("=")[1].encode()
-        
-        # read the content length
-        content_length = int(self.headers['Content-Length'])        
-        # read the raw POST data
-        post_data = self.rfile.read(content_length)
-        
-        # find the position of the boundary
-        boundary_pos = post_data.find(boundary)
-        image_data = post_data[boundary_pos:]
-        
-        # find the position of the image data within the boundary
-        image_pos = image_data.find(b'\r\n\r\n') + 4
-        
-        # extract the image data
-        image_data = image_data[image_pos:]
+        try:
+            # parse the content type header to get the boundary
+            content_type = self.headers['Content-Type']
+            boundary = content_type.split("=")[1].encode()
+            
+            # read the content length
+            content_length = int(self.headers['Content-Length'])        
+            # read the raw POST data
+            post_data = self.rfile.read(content_length)
+            
+            # find the position of the boundary
+            boundary_pos = post_data.find(boundary)
+            image_data = post_data[boundary_pos:]
+            
+            # find the position of the image data within the boundary
+            image_pos = image_data.find(b'\r\n\r\n') + 4
+            
+            # extract the image data
+            image_data = image_data[image_pos:]
 
-        # setup filename containing the timestamp
-        file_name = "Image " + datetime.now().strftime("%d-%m-%Y %H-%M-%S.%f") + ".jpg"
-        
-        # write the image data to a new file
-        with open(os.path.join(upload_dir, file_name), 'wb') as f:
-            f.write(image_data)
-        
-        # Send a response
-        self.send_response(200)
-        self.send_header('Content-type', 'text/plain')
-        # disable cors
-        self.send_header('Access-Control-Allow-Origin', '*')
-        self.end_headers()
-        self.wfile.write('{"msg": "Success"}'.encode())
+            # setup filename containing the timestamp
+            file_name = "Image " + datetime.now().strftime("%d-%m-%Y %H-%M-%S.%f") + ".jpg"
+            
+            # write the image data to a new file
+            with open(os.path.join(upload_dir, file_name), 'wb') as f:
+                f.write(image_data)
+            
+            # Send a response
+            self.send_response(200)
+            self.send_header('Content-type', 'text/plain')
+            # disable cors
+            self.send_header('Access-Control-Allow-Origin', '*')
+            self.end_headers()
+            self.wfile.write('{"msg": "Success"}'.encode())
+        except Exception as error:
+            print("FAIL FOR " + file_name + ":")
+            print(error)
+            print("--------------------------------------------------")
         
 def run_server(port=8000):
     server_address = ('', port)
